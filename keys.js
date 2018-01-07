c9dd90e68ddd155a83c283be92c30ea780b46ed7
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2018-01-07 16:43:05 -0500

    Minor change

--- keys.js
+++ keys.js
@@ -6,5 +6,3 @@ exports.twitterKeys = {
   access_token_key: '950096275055226881-66jYKuKSte4lXLpHrTlHBrADzule9qG',
   access_token_secret: 'xkhrMuQ7snwad0q6hnlggS21q6atLSLcAFztzmLRjpyWO',
 }
-
-module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -7,6 +7,9 @@
 	// Include the request npm package (REMEMBER: run "npm install request" in this folder first!)
 	var request = require("request");
 
+	//Code needed to grab the data from keys.js.
+	var keys = require("./keys.js");
+
 
 // MAIN PROCESS
 // ============
