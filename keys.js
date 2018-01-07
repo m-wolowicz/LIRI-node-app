e1fa47f521c1741644ba0c942ef2b399ae22d698
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2018-01-07 16:14:01 -0500

    Adding initial files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'hyNKmpcWUoCEyUAUQEbd6TpAN',
+  consumer_secret: 'BCXFSW6AqwgyieeKj3CzQZWZKPA0Ba9JfzpYXufyYkAGTgQgnO',
+  access_token_key: '950096275055226881-66jYKuKSte4lXLpHrTlHBrADzule9qG',
+  access_token_secret: 'xkhrMuQ7snwad0q6hnlggS21q6atLSLcAFztzmLRjpyWO',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,16 @@
+// GLOBAL VARIABLES
+// ================
+
+	// Includes the core node package for reading and writing files
+	var fs = require("fs");
+
+	// Include the request npm package (REMEMBER: run "npm install request" in this folder first!)
+	var request = require("request");
+
+
+// MAIN PROCESS
+// ============
+
+
+// FUNCTIONS
+// ============
