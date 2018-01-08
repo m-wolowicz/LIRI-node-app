2aad83e7fc1cfc1c26d7102b33b2780e57e3dde1
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2018-01-08 09:44:08 -0500

    App is working with user input. Still need to create each function

--- liri.js
+++ liri.js
@@ -1,19 +1,91 @@
 // GLOBAL VARIABLES
 // ================
 
-	// Includes the core node package for reading and writing files
-	var fs = require("fs");
+	// =======
+	// REQUIRE
+	// =======
+		// Includes the core node package for reading and writing files
+		var fs = require("fs");
 
-	// Include the request npm package (REMEMBER: run "npm install request" in this folder first!)
-	var request = require("request");
+		// Include the request npm package (REMEMBER: run "npm install request" in this folder first!)
+		var request = require("request");
 
-	//Code needed to grab the data from keys.js.
-	var keys = require("./keys.js");
+		//Code needed to grab the data from keys.js.
+		var keys = require("./keys.js");
+
+		//Include the node modules for twitter spotify
+		var Twitter = require("twitter");
+		var spotify = require("node-spotify-api");
+
+	// ===============
+	// INPUT VARIABLES
+	// ===============
+		//User Input for command parameter
+		var inputCommand = process.argv[2];
+		//User Input for song and movie titles
+		var inputTitles = process.argv[3];
 
 
 // MAIN PROCESS
 // ============
 
+	//Switch statement that calls each function depending on user input
+	switch (inputCommand) {
+
+		//Option 1: node liri.js my-tweets
+		case "my-tweets":
+			//Call Function for generating tweets from Twitter API
+			myTweets();
+			break;
+
+		//Option 2: node liri.js spotify-this-song '<song name here>'
+		case "spotify-this-song":
+			//Call Function for generating song info from Spotify API
+			spotifyThis();
+			break;
+
+		//Option 3: node liri.js movie-this '<movie name here>'
+		case "movie-this":
+			//Call Function for generating movie info from OMDB API
+			movieThis();
+			break;
+
+		//Option 4: node liri.js do-what-it-says
+		case "do-what-it-says":
+			//Call Function for generating random command in random.txt file
+			doWhatItSays();
+			break;
+
+		//Default "else" option, which console.logs the instructions to user
+		default: console.log(
+			'\n PLEASE RUN ONE OF THE COMMANDS BELOW:' +
+			'\n node liri.js my-tweets'  +
+			'\n node liri.js spotify-this-song "any song name"' +
+			'\n node liri.js movie-this "any movie name"' +
+			'\n node liri.js do-what-it-says' +
+			'\n'
+			);
+	}
 
 // FUNCTIONS
 // ============
+
+	//myTweets Function
+	function myTweets() {
+		console.log("...Retrieving your Tweets...");
+	}
+
+	//spotifyThis Function
+	function spotifyThis() {
+		console.log("...Retrieving your Song...");
+	}
+
+	//movieThis Function
+	function movieThis() {
+		console.log("...Retrieving your Mpvie...");
+	}
+
+	//doWhatItSays Function
+	function doWhatItSays() {
+		console.log("...Doing What It Says...");
+	}
