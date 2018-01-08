b499ce2a247ff5181131021233f1536100019d6b
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2018-01-08 11:26:45 -0500

    Created function for retrieving tweets, noe thw my-tweets command works.

--- liri.js
+++ liri.js
@@ -58,8 +58,9 @@
 
 		//Default "else" option, which console.logs the instructions to user
 		default: console.log(
+			'\n ==== THAT IS NOT A VALID COMMAND ====' +
 			'\n PLEASE RUN ONE OF THE COMMANDS BELOW:' +
-			'\n node liri.js my-tweets'  +
+			'\n node liri.js my-tweets' +
 			'\n node liri.js spotify-this-song "any song name"' +
 			'\n node liri.js movie-this "any movie name"' +
 			'\n node liri.js do-what-it-says' +
@@ -72,7 +73,41 @@
 
 	//myTweets Function
 	function myTweets() {
-		console.log("...Retrieving your Tweets...");
+		//Logging a separator to place the results under
+		console.log("\n==== HERE ARE YOUR TWEETS ====\n");
+
+		//Creating a new Twitter client using the keys file
+		var client = new Twitter({
+			consumer_key: keys.twitterKeys.consumer_key,
+			consumer_secret: keys.twitterKeys.consumer_secret,
+			access_token_key: keys.twitterKeys.access_token_key,
+			access_token_secret: keys.twitterKeys.access_token_secret
+		});
+
+		//Gather latest 20 tweets from @MelissaUM account:
+		var params = {screen_name: 'MelissaUM', count: 21};
+
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+			if (!error) {
+				//Create a for loop so that the same format is applied to each tweet
+				for (var i = 1; i < tweets.length; i++) {
+					//Variables containing each JSON data required
+					var timeCreated = tweets[i].created_at;
+					var tweetText = tweets[i].text;
+					//Variable containing format for each tweet
+					var output = 	"\n ---- Tweet Number " + i + " ----" +
+									"\n Tweet: " + tweetText +
+									"\n Posted on: " + timeCreated +
+									"\n --------------------------------" +
+									"\n";
+					//Log each tweet to the console.
+					console.log(output);
+				}
+			} else {
+				console.log(error);
+			}
+		});
 	}
 
 	//spotifyThis Function
